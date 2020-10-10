75b49553e2e04262a0006e7336b5e3f10a7efc14
Purnima <pehu.verma145@gmail.com>
2020-10-09 15:47:11 -1200

    class update

--- class.py
+++ class.py
@@ -36,3 +36,43 @@ myobj = MyClass('purnima','8700262044')
 myobj.mydetail()
 myobj.mypersionaldetail('python@gmail.com')
 # print(myobj.name)
+
+# we are trying to make a calculator using a class
+class calculator():
+    def __init__(self,a,b): # def is used as a constructor
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a + self.b
+    def sub(self):
+        return self.a - self.b 
+    def multiply(self):
+        return self.a * self.b 
+    def divide(self):
+        return self.a / self.b  
+
+a = int(input('Enter the first number :')) 
+b = int(input('Enter the second number :'))
+# now create a object 
+obj = calculator(a,b)
+while True:
+    def menu():
+        x=('1. Add\n2.Subtract \n3. Multiply\n4. Divide') 
+        print(x)
+    menu()
+    choice = int(input('please select one of the following :'))
+    if choice == 1:
+        print("Result:",obj.add())
+    elif choice == 2:
+        print("Result :",obj.sub())
+    elif choice == 3:
+        print("Result :",obj.multiply())   
+    elif choice == 4:
+        print("Result :",obj.divide())
+    elif choice == 0:
+        print("Again try one of the following")                                 
+        break
+    else:
+        print("Invaild option")
+        break
+    print()
